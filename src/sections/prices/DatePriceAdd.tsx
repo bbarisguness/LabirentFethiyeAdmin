26b9f59b3b56fc978b32c70afafe241e74908e5f
barış güneş <bbarisguness@msn.com>
2024-02-17 15:36:01 +0300

    price dates edit

--- src/sections/prices/DatePriceAdd.tsx
+++ src/sections/prices/DatePriceAdd.tsx
@@ -14,7 +14,7 @@ import { useFormik } from 'formik';
 import * as yup from 'yup';
 import useCreateDatePrice from 'hooks/priceDate/useCreateDatePrice';
 //import useCreateReservationInfo from 'hooks/reservation/useCreateReservationInfo';
-import {  useParams } from 'react-router';
+import { useParams } from 'react-router';
 
 //import useVillas from 'hooks/villa/useVillas';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
@@ -24,6 +24,7 @@ import useVillas from 'hooks/villa/useVillas';
 import { useState } from 'react';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import moment from 'moment';
+import apiRequest from 'services/request';
 
 //import { data } from 'data/org-chart';
 /**
@@ -40,14 +41,13 @@ const getInitialValues = () => {
     price: '',
     checkIn: '',
     checkOut: '',
-    villa:{}
+    villa: {}
   };
   return newReservation;
 };
 
 const DatePricesAdd = () => {
   const params = useParams();
-  
 
   const { data } = useVillas({
     Page: 1,
@@ -64,6 +64,11 @@ const DatePricesAdd = () => {
     validationSchema: validationSchema,
     onSubmit: (values, { setSubmitting }) => {
       try {
+        // console.log(moment(date1).add(-1,'days').format('YYYY-MM-DD').toString())
+        // return;
+        // console.log(moment(date1).format('YYYY-MM-DD').toString());
+        // console.log(moment(date2).format('YYYY-MM-DD').toString());
+
         if (params.id) {
           //values.villaId = params.id;
           values.villa = { connect: [params.id] };
@@ -73,6 +78,185 @@ const DatePricesAdd = () => {
 
         values.checkIn = moment(date1).format('YYYY-MM-DD').toString();
         values.checkOut = moment(date2).format('YYYY-MM-DD').toString();
+
+        apiRequest(
+          'GET',
+          `/price-dates?sort[0]=checkIn:asc&filters[$and][0][villa][id][$eq]=${params.id}&filters[$and][1][$or][0][$and][0][checkIn][$lte]=${values.checkIn}&filters[$and][1][$or][0][$and][1][checkOut][$gte]=${values.checkIn}&filters[$and][1][$or][1][$and][0][checkIn][$lte]=${values.checkOut}&filters[$and][1][$or][1][$and][1][checkOut][$gt]=${values.checkOut}&filters[$and][1][$or][2][$and][0][checkIn][$gt]=${values.checkIn}&filters[$and][1][$or][2][$and][1][checkOut][$lt]=${values.checkOut}&fields[0]=checkIn&fields[1]=checkOut&fields[2]=price&populate[villa][fields][0]=id&publicationState=live`
+        ).then((res) => {
+          //console.log('sorgu', res);
+          if (res.data.data.length > 0) {
+            const priceData = res.data.data;
+            var firstPrice = priceData[0];
+            var lastPrice = priceData[priceData.length - 1];
+            var endDate = firstPrice.attributes.checkOut;
+            //console.log('fonksiyon çalıştı');
+
+            if (firstPrice.attributes.checkIn === values.checkIn && firstPrice.attributes.checkOut === values.checkOut) {
+              // Sadece fiyat güncelle
+              // apiRequest('PUT', `/price-dates/${firstPrice.id}`, { data: { price: values.price } });
+              // dispatch(
+              //   openSnackbar({
+              //     open: true,
+              //     message: 'Fiyat başarıyla eklendi1',
+              //     variant: 'alert',
+              //     alert: {
+              //       color: 'success'
+              //     },
+              //     close: false
+              //   })
+              // );
+              apiRequest('DELETE', `/price-dates/${firstPrice.id}`);
+              //console.log('fonksiyon 1');
+            } else if (values.checkIn === firstPrice.attributes.checkIn && values.checkOut < firstPrice.attributes.checkOut) {
+              // apiRequest('POST', '/price-dates', {
+              //   data: { checkIn: values.checkIn, checkOut: values.checkOut, price: values.price, villa: { connect: [params.id] } }
+              // });
+              apiRequest('PUT', `price-dates/${firstPrice.id}`, {
+                data: { checkIn: moment(values.checkOut).add(1, 'days').format('YYYY-MM-DD').toString() }
+              });
+              dispatch(
+                openSnackbar({
+                  open: true,
+                  message: 'Fiyat başarıyla eklendi2',
+                  variant: 'alert',
+                  alert: {
+                    color: 'success'
+                  },
+                  close: false
+                })
+              );
+              //console.log('fonksiyon 2');
+            } else if (values.checkIn > firstPrice.attributes.checkIn && values.checkOut === firstPrice.attributes.checkOut) {
+              // apiRequest('POST', '/price-dates', {
+              //   data: { checkIn: values.checkIn, checkOut: values.checkOut, price: values.price, villa: { connect: [params.id] } }
+              // });
+              apiRequest('PUT', `price-dates/${firstPrice.id}`, {
+                data: { checkOut: moment(values.checkIn).add(-1, 'days').format('YYYY-MM-DD').toString() }
+              });
+              dispatch(
+                openSnackbar({
+                  open: true,
+                  message: 'Fiyat başarıyla eklendi3',
+                  variant: 'alert',
+                  alert: {
+                    color: 'success'
+                  },
+                  close: false
+                })
+              );
+              //console.log('fonksiyon 3');
+            } else if (values.checkIn > firstPrice.attributes.checkIn && values.checkOut < firstPrice.attributes.checkOut) {
+              // apiRequest('POST', '/price-dates', {
+              //   data: { checkIn: values.checkIn, checkOut: values.checkOut, price: values.price, villa: { connect: [params.id] } }
+              // });
+              apiRequest('PUT', `price-dates/${firstPrice.id}`, {
+                data: { checkOut: moment(values.checkIn).add(-1, 'days').format('YYYY-MM-DD').toString() }
+              });
+              apiRequest('POST', '/price-dates', {
+                data: {
+                  checkIn: moment(values.checkOut).add(1, 'days').format('YYYY-MM-DD').toString(),
+                  checkOut: endDate,
+                  price: firstPrice.attributes.price,
+                  villa: { connect: [params.id] }
+                }
+              });
+              dispatch(
+                openSnackbar({
+                  open: true,
+                  message: 'Fiyat başarıyla eklendi4',
+                  variant: 'alert',
+                  alert: {
+                    color: 'success'
+                  },
+                  close: false
+                })
+              );
+              //console.log('fonksiyon 4');
+            } else if (
+              values.checkIn < firstPrice.attributes.checkOut &&
+              values.checkIn > firstPrice.attributes.checkIn &&
+              values.checkOut < lastPrice.attributes.checkOut &&
+              values.checkOut > lastPrice.attributes.checkIn
+            ) {
+              // apiRequest('POST', '/price-dates', {
+              //   data: { checkIn: values.checkIn, checkOut: values.checkOut, price: values.price, villa: { connect: [params.id] } }
+              // });
+              apiRequest('PUT', `price-dates/${firstPrice.id}`, {
+                data: { checkOut: moment(values.checkIn).add(-1, 'days').format('YYYY-MM-DD').toString() }
+              });
+              apiRequest('PUT', `price-dates/${lastPrice.id}`, {
+                data: { checkIn: moment(values.checkOut).add(1, 'days').format('YYYY-MM-DD').toString() }
+              });
+              dispatch(
+                openSnackbar({
+                  open: true,
+                  message: 'Fiyat başarıyla eklendi5',
+                  variant: 'alert',
+                  alert: {
+                    color: 'success'
+                  },
+                  close: false
+                })
+              );
+              //console.log('fonksiyon 5');
+            } else if (values.checkIn < firstPrice.attributes.checkOut && values.checkOut > firstPrice.attributes.checkOut) {
+              // apiRequest('POST', '/price-dates', {
+              //   data: { checkIn: values.checkIn, checkOut: values.checkOut, price: values.price, villa: { connect: [params.id] } }
+              // });
+              apiRequest('PUT', `price-dates/${firstPrice.id}`, {
+                data: { checkOut: moment(values.checkIn).add(-1, 'days').format('YYYY-MM-DD').toString() }
+              });
+              dispatch(
+                openSnackbar({
+                  open: true,
+                  message: 'Fiyat başarıyla eklendi6',
+                  variant: 'alert',
+                  alert: {
+                    color: 'success'
+                  },
+                  close: false
+                })
+              );
+              //console.log('fonksiyon 6');
+            } else if (
+              values.checkIn < firstPrice.attributes.checkIn &&
+              values.checkOut >= firstPrice.attributes.checkIn &&
+              values.checkOut < firstPrice.attributes.checkOut
+            ) {
+              apiRequest('PUT', `price-dates/${firstPrice.id}`, {
+                data: { checkIn: moment(values.checkOut).add(1, 'days').format('YYYY-MM-DD').toString() }
+              });
+              dispatch(
+                openSnackbar({
+                  open: true,
+                  message: 'Fiyat başarıyla eklendi',
+                  variant: 'alert',
+                  alert: {
+                    color: 'success'
+                  },
+                  close: false
+                })
+              );
+              //console.log('fonksiyon 7');
+            } else if (values.checkIn === firstPrice.attributes.checkOut) {
+              apiRequest('PUT', `/price-dates/${firstPrice.id}`, {
+                data: { checkOut: moment(firstPrice.attributes.checkOut).add(-1, 'days').format('YYYY-MM-DD').toString() }
+              });
+              dispatch(
+                openSnackbar({
+                  open: true,
+                  message: 'Fiyat başarıyla eklendi',
+                  variant: 'alert',
+                  alert: {
+                    color: 'success'
+                  },
+                  close: false
+                })
+              );
+              //console.log('fonksiyon 8');
+            }
+          }
+        });
 
         mutate(
           {
@@ -93,13 +277,17 @@ const DatePricesAdd = () => {
               );
             },
             onSuccess: (res) => {
-              // createReservationInfo({data:info}, {
-              //   onError:(){},
-              //   onSuccess: (res) => {}
-              // })
-
-              //navigate('/villa/show/' + params.id + '/price');
-              
+              dispatch(
+                openSnackbar({
+                  open: true,
+                  message: 'Fiyat başarıyla eklendi',
+                  variant: 'alert',
+                  alert: {
+                    color: 'success'
+                  },
+                  close: false
+                })
+              );
               setSubmitting(false);
             }
           }
